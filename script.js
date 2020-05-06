fe2fb8b71a337fa09b6384e11e60934a2bb06c94
dmitrii-milk <58372916+dmitrii-milk@users.noreply.github.com>
2020-05-06 11:02:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function drawChess() {
+    let mainBlock = document.querySelector('.main-board');
+
+    let block;
+
+    let flag = true;
+
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+
+            if (j == 0) {
+                flag = !flag;
+            }
+
+            block = document.createElement('div');
+
+
+            if (flag) {
+                block.className = 'block black';
+            } else {
+                block.className = 'block white';
+            }
+
+            mainBlock.append(block);
+
+            flag = !flag;
+
+        }
+
+
+    }
+
+
+}
+
+drawChess();
